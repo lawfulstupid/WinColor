69272f7f8a43bb6bc32d063800eb1d0e59ad6c40
Alex Bradley <a.bradley.dm@gmail.com>
2019-03-18 19:56:57 +0000

    totally redone

--- Main.hs
+++ Main.hs
@@ -8,27 +8,37 @@ import Data.List (foldl')
 import System.Environment (getArgs)
 import Data.Char (toUpper)
 
-data Color = Black | Red | Green | Yellow | Blue | Purple | Magenta | Cyan | White
+data Color = Black | Red | Green | Yellow | Blue | Purple | Cyan | White
     deriving (Enum)
     
 type ColorProfile = Maybe (Color, Bool) -- Bool represents bold
     
-data TextEffect = FX
-   { underline :: Bool
-   , inverse   :: Bool
-   , fgcolor   :: ColorProfile
-   , bgcolor   :: ColorProfile }
+-- data TextEffect = FX
+   -- { underline :: Bool
+   -- , inverse   :: Bool
+   -- , fgcolor   :: ColorProfile
+   -- , bgcolor   :: ColorProfile }
 
--- The base 'do nothing' effect
-fx :: TextEffect
-fx = FX False False Nothing Nothing
+data Bold = Dim | Bright | Dark | Light | Weak | Bold
+   deriving (Eq)
 
-main :: IO ()
-main = do
-   args <- getArgs
-   if null args || elem "/h" args || elem "/H" args
-   then mapM_ putStrLn help
-   else print $ show $ apply parserFX $ unwords args
+instance Enum Bold where
+   fromEnum b = if elem b [Dim,Dark,Weak] then 0 else 1
+   toEnum 0 = Dim
+   toEnum 1 = Bright
+
+data TextEffect = Reset
+   | Invert
+   | Underline
+   | FG Bold Color
+   | BG Bold Color
+
+-- main :: IO ()
+-- main = do
+   -- args <- getArgs
+   -- if null args || elem "/h" args || elem "/H" args
+   -- then mapM_ putStrLn help
+   -- else print $ show $ apply parserFX $ unwords args
 
 help :: [String]
 help =
@@ -40,60 +50,26 @@ help =
    , "\t/H\tShow this help screen."
    ]
 
-parserFX :: Parser TextEffect
-parserFX = do
-   let f = fmap (toUpper . head) $ matchOne "0123456789ABCDEFabcdef" 
-   colors <- mfilter ((<= 2) . length) $ many f
-   flags  <- many (whitespace >> matchOne ["/U", "/I", "/u", "/i"])
-   let flag_ul = elem "/U" flags || elem "/u" flags
-   let flag_iv = elem "/I" flags || elem "/i" flags
-   let fx' = fx { underline = flag_ul, inverse = flag_iv }
-   return $ case map oldToNew colors of
-      []      -> fx'
-      [fg]    -> fx' { fgcolor = fg }
-      [fg,bg] -> fx' { fgcolor = fg, bgcolor = bg }
-
-oldToNew :: Char -> ColorProfile
-oldToNew = \case
-   '0' -> Just (Black, False)
-   '8' -> Just (Black, True)
-   '1' -> Just (Blue, False)
-   '9' -> Just (Blue, True)
-   '2' -> Just (Green, False)
-   'A' -> Just (Green, True)
-   '3' -> Just (Cyan, False)
-   'B' -> Just (Cyan, True)
-   '4' -> Just (Red, False)
-   'C' -> Just (Red, True)
-   '5' -> Just (Purple, False)
-   'D' -> Just (Purple, True)
-   '6' -> Just (Yellow, False)
-   'E' -> Just (Yellow, True)
-   '7' -> Just (White, False)
-   'F' -> Just (White, True)
-   _   -> Nothing
+-- parserFX :: Parser TextEffect
+-- parserFX = do
+   -- let f = fmap (toUpper . head) $ matchOne "0123456789ABCDEFabcdef" 
+   -- colors <- mfilter ((<= 2) . length) $ many f
+   -- flags  <- many (whitespace >> matchOne ["/U", "/I", "/u", "/i"])
+   -- let flag_ul = elem "/U" flags || elem "/u" flags
+   -- let flag_iv = elem "/I" flags || elem "/i" flags
+   -- let fx' = fx { underline = flag_ul, inverse = flag_iv }
+   -- return $ case map oldToNew colors of
+      -- []      -> fx'
+      -- [fg]    -> fx' { fgcolor = fg }
+      -- [fg,bg] -> fx' { fgcolor = fg, bgcolor = bg }
    
 
 instance Show TextEffect where
-   show (FX ul iv fg bg) = handleBG ++ handleFG fg ++ handleIV ++ handleUL
-      $> foldl' (\ x y -> x ++ ";" ++ show y) ""
-      $> tail
-      $> ("\ESC[" ++)
-      $> (++ "m")
+   show effect = "\ESC[" ++ handle effect ++ "m"
       where
-        
-      handleBG :: [Int]
-      handleBG = case bg of
-         Nothing    -> []
-         Just (c,b) -> handleFG bg ++ [7]    -- assign to FG then swap in
-        
-      handleFG :: ColorProfile -> [Int]
-      handleFG = \case
-         Nothing    -> []
-         Just (c,b) -> [fromEnum b, fromEnum c]
-        
-      handleIV :: [Int]
-      handleIV = if iv then [7] else []
-        
-      handleUL :: [Int]
-      handleUL = if ul then [4] else []
+      handle = \case
+         Reset -> ""
+         Invert -> "7"
+         Underline -> "4"
+         (BG b c) -> shows (fromEnum b) ";" ++ shows (fromEnum c + 30) ";7"
+         (FG b c) -> shows (fromEnum b) ";" ++ show (fromEnum c + 30)
